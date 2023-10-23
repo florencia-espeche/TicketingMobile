3790f325ad5074e4ebb3208ab074169129b32f78
Florencia <flor.espeche@gmail.com>
2023-10-23 17:13:27 -0300

    add navigation goBack reviewDetails to home

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,11 +1,16 @@
 import { globalStyles } from "../styles";
-
-import { View, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { View, Text, Button } from "react-native";
 
 const ReviewDetails = () => {
+  const navigation = useNavigation();
+  const pressHandler = () => {
+    navigation.goBack();
+  };
   return (
     <View style={globalStyles.container}>
       <Text>Review Details Screen</Text>
+      <Button title="back to home screen" onPress={pressHandler} />
     </View>
   );
 };
